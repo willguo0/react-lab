297b5e631f1f3b5e894b247557ed17b641cf7135
willguo0 <guo.william2323@gmail.com>
2022-11-09 19:17:29 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,28 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState({});
+  const [total, setTotal] = useState("Nothing's Here Yet Loser");
+
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Expired Foods Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem total= {total} setTotal = {setTotal}name={item.name} description={item.description} price={item.price} image = {item.image} cart={cart} setCart = {setCart}/> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {Object.keys(cart).map((item, index) => (
+          <div> 
+          <h3>{item}: {cart[item]}x</h3>
+          <h3></h3>
+          </div>
+        ))}
+        <h2>Total</h2>
+        {total}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,31 @@
 // TODO: create a component that displays a single bakery item
+import { useState, React } from "react";
+
+const BakeryItem = ({ name, description, price, image, cart,setCart, total, setTotal}) => {
+    function setDict(cart, setCart, name) {
+        if (typeof total === 'string' || total instanceof String){
+            console.log('bruh')
+            setTotal(price)
+        }else{
+        setTotal(total+price)
+        }
+        if (name in cart){
+            setCart({...cart, [name]:cart[name]+1})
+        }else{
+            console.log('pls')
+            setCart({...cart, [name]:1})
+        }
+    }
+    return(
+    
+    <div>
+        <img src={image}></img>
+        <div>{name}: ${price.toFixed(2)}</div>
+        <div>{description}</div>
+        
+        
+        <button onClick={()=>setDict(cart,setCart,name)}>Add to Cart</button>
+        </div>
+    )
+}
+export default BakeryItem;
